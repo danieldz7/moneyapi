c148e537b412d5a2d7ecaebc3dfbb238b669d603
Daniel Pereira <danieldz7@msn.com>
2020-06-08 13:41:14 -0300

    Initial commit

---
+++ src/test/java/com/pereiracorp/money/api/MoneyapiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.pereiracorp.money.api;
+
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MoneyapiApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
